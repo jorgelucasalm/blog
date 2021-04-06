6431ae5e8403e3f6977fd924c7b7635b96903034
Jorge Lucas <jorge.lucasalm@gmail.com>
2021-04-06 01:08:34 -0300

    add: create article form

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/articles", (req, res)=>{
-    res.send("articles")
+router.get("/admin/articles/new", (req, res)=>{
+    res.render("./admin/articles/new")
 });
 
 module.exports = router;

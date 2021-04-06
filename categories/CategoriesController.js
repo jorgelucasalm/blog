b01483be1822471fd33d8745ca3174f32d0bcbaa
Jorge Lucas <jorge.lucasalm@gmail.com>
2021-04-06 01:02:24 -0300

    feat: add update category

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -29,29 +29,62 @@ router.post("/categories/save", (req, res) => {
 
 router.get("/admin/categories", (req, res) => {
     Category.findAll().then(categories => {
-        res.render("./admin/categories/index", {categories: categories})
+        res.render("./admin/categories/index", { categories: categories })
     })
 })
 
-router.post("/categories/delete", (req, res)=>{
+router.post("/categories/delete", (req, res) => {
     var id = req.body.id;
-    if(id != undefined){
-        if(!isNaN(id)){
+    if (id != undefined) {
+        if (!isNaN(id)) {
 
             Category.destroy({
                 where: {
                     id: id
                 }
-            }).then(()=>{
+            }).then(() => {
                 res.redirect("/admin/categories")
             })
 
-        }else{
+        } else {
             res.redirect("/admin/categories")
         }
-    }else{
+    } else {
         res.redirect("/admin/categories")
     }
-})
+});
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+    var id = req.params.id;
+
+    if(isNaN(id)){
+        res.redirect("/admin/categories");
+    }
+
+    Category.findByPk(id).then(category => {
+        if (category != undefined) {
+            res.render("admin/categories/edit", {category:category})
+        } else {
+            res.redirect("/admin/categories")
+        }
+    }).catch(err =>{
+        res.redirect("/admin/categories")
+    })
+});
+
+router.post("/categories/update", (req, res)=>{
+    var id = req.body.id;
+    var title = req.body.title;
+
+    Category.update({title: title, slug: slugify(title)}, {
+        where: {
+            id: id
+        }
+    }).then(()=>{
+        res.redirect("/admin/categories")
+    })
+});
+
+
 
 module.exports = router;

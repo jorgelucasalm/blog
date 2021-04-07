9c14ee3ca61420adff53e57aa5619f4090fe857d
Jorge Lucas <jorge.lucasalm@gmail.com>
2021-04-07 16:19:22 -0300

    feat: add save article

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,8 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
+
+router.use(express.urlencoded({ extended: true }));
 
 router.get("/admin/articles/new", (req, res)=>{
-    res.render("./admin/articles/new")
+    Category.findAll().then(categories=>{
+        res.render("./admin/articles/new", {categories: categories})
+    })
+    
+});
+
+router.post("/articles/save", (req, res)=>{
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category  
+    }).then(()=>{
+        res.redirect("/admin/articles/new")
+    });
+    
 });
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize('guiapress','root','', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '-03:00'
 });
 
 module.exports = sequelize;

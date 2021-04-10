170063c7b5289cefcff98fd9ab552e8fcb164c45
Jorge Lucas <jorge.lucasalm@gmail.com>
2021-04-10 20:11:07 -0300

    feat: add save user

--- user/UserController.js
+++ user/UserController.js
@@ -11,5 +11,22 @@ router.get("/admin/users/create", (req, res) => {
     res.render("./admin/users/create")
 });
 
+router.post("/users/save", (req, res) => {
+    var email = req.body.email;
+    var pass = req.body.pass;
+
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(pass, salt)
+
+    User.create({
+        login: email,
+        password: hash
+    }).then(() => {
+        res.redirect("/");
+    }).catch((err)=>{
+        res.redirect("/")
+    })
+
+});
 
 module.exports = router;
